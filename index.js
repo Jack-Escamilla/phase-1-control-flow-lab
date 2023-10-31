acf0a2d5ae4ab4e75342d157fef611a9a2a7dab4
Jack Escamilla <jack.escamilla@gmail.com>
2023-10-31 12:42:19 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+  if (distance <= 200){
+    return "This one is on me!"
+  }
+
+  if (distance >= 400 && distance<= 2000){
+    return "That will be twenty bucks."
+  }
+
+  if (distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+
+  if (distance > 2500)
+
+    return "No can do."
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(theCity){
+  
+  return theCity === "NYC" ? "Ok, sounds good." : "No go.";
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(theTip){
+let theTipMessage;
+  switch (theTip) {
+    case 'generous':
+        theTipMessage = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        theTipMessage = 'Thank you.';
+        break;
+    default:
+        theTipMessage = 'Bye.';
+        break;
+}
+return theTipMessage;
 }
